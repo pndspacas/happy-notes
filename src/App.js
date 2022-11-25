e7f0e25000a2796d8396f37f549977e7c4589290
Pedro <pndspacas@gmail.com>
2022-11-24 21:21:44 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
